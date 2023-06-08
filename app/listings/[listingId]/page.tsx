0542293d50a60a64d8bb5d4553244c80be592dd3
Fabio.MA.Pereira <fabio.ma.pereira@edu.azores.gov.pt>
2023-06-08 23:52:09 +0000

    changes

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -16,6 +16,7 @@ const ListingPage = async ({ params }: { params: IParams }) => {
 
   const listing = await getListingById(params);
   const reservations = await getReservations(params);
+
   const currentUser = await getCurrentUser();
 
   if (!listing) {
